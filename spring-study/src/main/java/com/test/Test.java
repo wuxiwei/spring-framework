58dae71899bc404eea7f317134f35765c66c082b
wuxiwei <wxw_it@163.com>
2020-03-19 01:09:30 +0800

    test

--- spring-study/src/main/java/com/test/Test.java
+++ spring-study/src/main/java/com/test/Test.java
@@ -3,8 +3,11 @@ package com.test;
 import com.test.api.A;
 import com.test.api.B;
 import com.test.impl.UserServiceImpl;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * @author wuxiwei
@@ -18,7 +21,12 @@ public class Test {
 //		UserServiceImpl bean = context.getBean(UserServiceImpl.class);
 //		System.out.println(bean.hello());
 
+		XmlBeanFactory bf = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
+		A a = (A) bf.getBean("a");
+		B b = (B) bf.getBean("b");
 
+		System.out.println(a.get());
+		System.out.println(b.get());
 
 
 	}
